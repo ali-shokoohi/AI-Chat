146099c67ae5e4badd9805771a8f83fe9ae6bac5
Ali Shokoohi <alishokoohi666@gmail.com>
2019-07-24 01:47:41 +0430

    Logging the logs :)

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from telegram.client import Telegram
 from datetime import datetime
+import logging
+
+logging.basicConfig(filename='./logs/app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("root")
+logger.warning('This is a Warning')
 
 tg = Telegram(
     api_id="649592",
@@ -41,7 +46,7 @@ def main():
 
             return True
         except Exception as e:
-            print("A exeption has been found:", e)
+            logger.error("A exeption has been found:", exc_info=True)
             return False
 
     #Insert new data message
@@ -75,7 +80,7 @@ def main():
             
             return True
         except Exception as e:
-            print("A exeption has been found:", e)
+            logger.error("A exeption has been found:", exc_info=True)
             return False
         
 
@@ -97,7 +102,7 @@ def main():
             reply_to_message_id = message["reply_to_message_id"]
             message_content = message["content"].get("text", {})
             message_text = message_content.get("text", "").lower()
-#            print(f"A text message has been received from {chat_id} at {datetime.now()} and that is:\n{message_text}")
+            logger.warning(f"A text message has been received from {chat_id} at {datetime.now()} and that is:\n{message_text}")
             if reply_to_message_id > 0:
                 answer = "Hello human as reply!"
                 to_message = tg.get_message(chat_id=chat_id, message_id=reply_to_message_id)
@@ -120,7 +125,7 @@ def main():
         if (user_id != me):
             photo_content = update["message"]["content"].get("photo", {})
             photo_id = photo_content.get("id", "").lower()
-#            print(f"A photo message has been received from {chat_id} at {datetime.now()} and ID of that is:\n{photo_id}")
+            logger.warning(f"A photo message has been received from {chat_id} at {datetime.now()} and ID of that is:\n{photo_id}")
             
             if is_private(chat_id):
                 tg.send_message(
@@ -132,27 +137,28 @@ def main():
     def unknown_handler(update):
         user_id = update["message"]["sender_user_id"]
         chat_id = update["message"]["chat_id"]
-        if (user_id != me) and is_private(chat_id):
-#            print(f"A unknown message has been received from {chat_id} at {datetime.now()}")
-            tg.send_message(
-                chat_id=chat_id,
-                text="What is this human?!",
-            )
+        if (user_id != me):
+            logger.warning(f"A unknown message has been received from {chat_id} at {datetime.now()}")
+            if is_private(chat_id):
+                tg.send_message(
+                    chat_id=chat_id,
+                    text="What is this human?!",
+                )
     
     #A function for handlling new messages
     def new_message_handler(update):
-        print(data)
+        logger.warning(str(data))
         message_type = update["message"]["content"]["@type"]
         if message_type == "messageText":#If message is a text
             message_handler(update)
         elif message_type == "messagePhoto":#If message is a photo
             photo_handler(update)
-#        elif message_type == ...:
-#            ..._handler(update)
+    #        elif message_type == ...:
+    #            ..._handler(update)
         else:#If messgae is a unknown type
             unknown_handler(update)
 
-    print(result.update)
+    logger.warning(result.update)
     tg.add_message_handler(new_message_handler)
     tg.idle()  # blocking waiting for CTRL+C
 
