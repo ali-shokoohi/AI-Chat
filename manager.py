4eec7b5c56566d59df9e8248cdae61e4ec703af4
Ali Shokoohi <alishokoohi666@gmail.com>
2019-07-26 01:46:56 +0430

    Rename & Add Message class

--- manager.py
+++ manager.py
@@ -1,5 +1,31 @@
 import sqlite3
 
+class Message:
+    def __init__(self, id=0, text="", date=0.0, reply_to_id=0):
+        self.id = id
+        self.text = text
+        self.date = date
+        self.reply_to_id = reply_to_id
+
+    def loads(self, msg):
+        self.id = msg["id"]
+        self.date = msg["date"]
+        self.reply_to_id = msg["reply_to_message_id"]
+        self.text = msg["content"]["text"]["text"]
+    
+    def dumps(self, message):
+        msg = {
+            "id": self.id,
+            "date": self.date,
+            "reply_to_message_id": self.reply_to_id,
+            "content": {
+                "text": {
+                    "text": self.text
+                }
+            }
+        }
+        return msg        
+
 class Data:
     def __init__(self, location="./database/data.db"):
         #Connect to database
@@ -17,5 +43,5 @@ class Data:
             )"""
         )
         self.conn.commit()
-    def __exit__():
+    def __exit__(self):
         self.conn.close()
